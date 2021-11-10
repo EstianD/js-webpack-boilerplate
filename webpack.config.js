e971785223f2c23215d3656f83481fe74dc5a861
EstianD <code.estian@gmail.com>
2021-11-10 15:14:32 +0200

    Added webpack config for media files

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,30 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: "file-loader",
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              disable: true, // Disables on development mode
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        use: "file-loader",
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       hash: true,
