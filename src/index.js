3b195ae4c5f77057e4b33206ba7dbdc5b81d9d7d
EstianD <code.estian@gmail.com>
2021-11-10 19:28:24 +0200

    Added image support in js. Html img tags not working yet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import axios from "axios";
+import someImage from "./images/earth.png";
 import "./style.css";
 
 axios
   .get("https://jsonplaceholder.typicode.com/todos/1")
   .then((response) => console.log(response));
+
+function component() {
+  const image = new Image();
+  image.src = someImage;
+
+  const element = document.createElement("div");
+
+  element.appendChild(image);
+
+  return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   module: {
     rules: [
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: "file-loader",
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              disable: true, // Disables on development mode
-            },
-          },
-        ],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
       {
         test: /\.(ogg|mp3|wav|mpe?g)$/i,
