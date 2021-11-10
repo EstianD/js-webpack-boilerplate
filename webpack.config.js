273ab9e2b9367bec5031d88ef47032bc4497b16a
EstianD <code.estian@gmail.com>
2021-11-10 14:35:17 +0200

    second commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: "JS Webpack Boilerplate",
+      header: "js webpack title",
+      metaDesc: "Webpack example description",
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  mode: "development",
+  output: {
+    clean: true,
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    open: true,
+  },
+};
